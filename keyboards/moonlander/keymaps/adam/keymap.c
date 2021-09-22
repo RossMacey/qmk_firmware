002c9b2f7f2a36c804bc3ff251638ded458e82f7
Adam Damiano <onaimada@gmail.com>
2021-09-21 19:37:19 -0700

    Make minor improvements
    - Allow right shift to work for making curly braces in the _NAV layer
    - Add a _SYMB toggle instead of backspace
    - Add tab navigation to _NAV

--- keyboards/moonlander/keymaps/adam/keymap.c
+++ keyboards/moonlander/keymaps/adam/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LGUI,W_HM_A, W_HM_S,  W_HM_D,  W_HM_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H, W_HM_J,  W_HM_K,  W_HM_L, W_HM_SCLN, LT(_MDIA, KC_QUOT),
         KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  LCTL_T(KC_SLSH), KC_RSFT,
         KC_LCTL,KC_LGUI,KC_LALT,KC_LEFT,TT(_SYMB),  KC_LALT,                      LCTL_T(KC_ESC),   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(_SYMB),
-                                            KC_SPC,  KC_LCTL, KC_LGUI,           KC_ENT,  KC_BSPC, MO(_NAV)
+                                            KC_SPC,  KC_LCTL, KC_LGUI,           KC_ENT,  TT(_SYMB), MO(_NAV)
     ),
     [_MAC_BASE] = LAYOUT_moonlander(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,              KC_EQL, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_HYPR, M_HM_A,M_HM_S,  M_HM_D,  M_HM_F,    KC_G,    KC_HYPR,           KC_MEH, KC_H,  M_HM_J,  M_HM_K,  M_HM_L,  M_HM_SCLN, LT(_MDIA, KC_QUOT),
         KC_LSFT, LGUI_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  LGUI_T(KC_SLSH), KC_RSFT,
         KC_LCTL,KC_LGUI,KC_LALT,KC_LEFT,TT(_SYMB),  KC_LALT,                      LGUI_T(KC_ESC),   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(_SYMB),
-                                            KC_SPC,  KC_LGUI, KC_LALT,           KC_ENT,  KC_BSPC, MO(_NAV)
+                                            KC_SPC,  KC_LGUI, KC_LALT,           KC_ENT,  TT(_SYMB), MO(_NAV)
     ),
     [_GAMING] = LAYOUT_moonlander(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,              KC_EQL, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
@@ -83,16 +83,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_9,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_8,              KC_MEH, KC_H,   KC_J,    KC_K,    KC_L,  KC_SCLN, LT(_MDIA, KC_QUOT),
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,   KC_M, KC_COMM,  KC_DOT,LCTL_T(KC_SLSH), KC_RSFT,
         KC_LCTL,KC_LGUI,KC_LALT,KC_LEFT,TT(_SYMB),  KC_LALT,                      LCTL_T(KC_ESC),   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(_SYMB),
-                                            KC_SPC,  KC_LCTL, KC_LGUI,           KC_ENT,  KC_BSPC, MO(_NAV)
+                                            KC_SPC,  KC_LCTL, KC_LGUI,           KC_ENT,  TT(_SYMB), MO(_NAV)
     ),
 
+    // Having numpad-specific numbers (e.g. KC_P1 instead of KC_1) isn't
+    // important for almost anything other than typing alt codes on Windows. I
+    // opted for regular number keys so that I could use win+# more naturally to
+    // move between open applications.
     [_SYMB] = LAYOUT_moonlander(
         KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,         _______, KC_F6,   KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,
-        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______,KC_NLCK,KC_P7,    KC_P8,    KC_P9,    KC_PAST, KC_F12,
-        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_PMNS, KC_P4,    KC_P5,    KC_P6,    KC_PPLS, _______,
-        _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_P1,    KC_P2,    KC_P3,    KC_BSLS, _______,
-        _______, _______, _______, _______, TG(_SYMB),          _______,           _______,          KC_P0,    KC_P0,   KC_PDOT,   KC_PEQL, TG(_SYMB),
-                                            _______, _______, _______,           _______,_______, _______
+        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______,KC_NLCK,  KC_7,     KC_8,      KC_9,    KC_PAST, KC_F12,
+        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_PMNS, KC_4,     KC_5,      KC_6,    KC_PPLS, _______,
+        _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_COMM, KC_1,     KC_2,      KC_3,    KC_BSLS, _______,
+        _______, _______, _______, _______, TG(_SYMB),        _______,           _______,          KC_0,     KC_0,   KC_PDOT,    KC_PEQL, TG(_SYMB),
+                                            _______, _______, _______,           _______,TG(_SYMB), _______
     ),
 
     [_NAV] = LAYOUT_moonlander(
@@ -100,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,           _______,KC_PGUP,KC_HOME, KC_UP,   KC_END,  KC_TAB, _______,
         _______, _______, _______, _______, _______, _______, _______,           _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______,                             KC_ESC,  KC_BSPC, KC_ENT,  KC_DEL,  _______, _______,
-        _______, _______, _______, _______, _______,         _______,            _______,          _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______,         _______,            _______,          C(S(KC_TAB)),C(KC_TAB), _______, _______, _______,
                                             _______, _______, _______,           _______, _______, _______
     ),
 
@@ -157,6 +161,7 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             }
 
             rgb_matrix_set_color(68, 0x64, 0x00, 0x64);  // Rightmost RH piano key
+            rgb_matrix_set_color(69, 0x00, 0x64, 0x00);  // Next piano key
             break;
         case _MAC_BASE:
             rgb_matrix_set_color(22, 0x64, 0x64, 0x64);  // 'F' key
@@ -165,6 +170,9 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         case _GAMING:
             set_color_for_contiguous_keycodes(0, 31, RGB_RED);
             rgb_matrix_set_color(66, RGB_RED);  // Leave gaming mode
+
+            const uint8_t wasd_keycodes[] = {11, 7, 12, 17};
+            set_all_keys_colors(wasd_keycodes, sizeof(wasd_keycodes) / sizeof(uint8_t), RGB_GREEN);
             break;
         case _SYMB: {
             rgb_matrix_set_color(62, 0x64, 0x00, 0x00);  // 'Y' key
@@ -228,7 +236,7 @@ void keyboard_post_init_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     bool isPressed   = record->event.pressed;
     bool isCtrlHeld  = get_mods() & MOD_BIT(KC_LCTL);
-    bool isShiftHeld = get_mods() & MOD_BIT(KC_LSFT);
+    bool isShiftHeld = (get_mods() & MOD_BIT(KC_LSFT)) || (get_mods() & MOD_BIT(KC_RSFT));
 
     switch (keycode) {
         // case MAC_SWP:
