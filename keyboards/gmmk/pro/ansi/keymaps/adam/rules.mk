b617feefd8ea5803284806c71a8c57eae0d38a47
Adam Damiano <onaimada@gmail.com>
2023-06-18 20:44:36 -0700

    Update GMMK Pro layout
    I still use it for gaming, but I'm having issues with double and triple
    presses. Unfortunately, this doesn't actually fix that problem, but I
    cleaned up some out-of-date #defines.

--- keyboards/gmmk/pro/ansi/keymaps/adam/rules.mk
+++ keyboards/gmmk/pro/ansi/keymaps/adam/rules.mk
@@ -1,4 +1,5 @@
 # https://docs.qmk.fm/#/feature_combo
 # COMBO_ENABLE = yes
 
-
+# https://www.reddit.com/r/glorious/comments/qidnto/the_best_way_to_fix_gmmk_pro_input_latency_with
+DEBOUNCE_TYPE = asym_eager_defer_pk
